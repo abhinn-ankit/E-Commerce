daae2d2748e81f1113a09e4fbd3d9ad578be3116
YuchenWang <wang.yuchen3@husky.neu.edu>
2017-12-14 16:01:43 -0500

    wyc

--- FinalProject/src/app/view-order/view-order.component.ts
+++ FinalProject/src/app/view-order/view-order.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, ParamMap} from '@angular/router';
+import { OrderService } from '../services/order.service';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-view-order',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewOrderComponent implements OnInit {
 
-  constructor() { }
+  orderID:number;
+  products: Product[];
+
+  constructor(private route: ActivatedRoute,
+              private orderService: OrderService,
+              private router: Router,
+              private productService: ProductService) { }
 
   ngOnInit() {
+    this.route.paramMap
+    .switchMap((params: ParamMap) => params.getAll('orderID'))
+    .subscribe(orderID => {
+      console.log(orderID);
+      this.orderID = +orderID;
+      this.orderService.getOrder(orderID)
+      .subscribe(order => {
+        for(let i=0;i<order.productList.length;i++){
+          this.productService.getProduct(order.productList[i].productId)
+              .subscribe(product => {
+                this.products.push(product);
+              })
+        }
+      })
+    })
   }
 
 }
