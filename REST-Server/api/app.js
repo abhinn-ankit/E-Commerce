aba64ddf0f88ee984f60c25d482d8cf85a854192
Abhinn Ankit <ankit.a@husky.neu.edu>
2018-01-19 18:11:59 -0500

    Removed unnecessary project

--- REST-Server/api/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = function(app) {
-  let accountModel = require('./models/account');
-
-  let accountdRoutes = require('./routes/account-route');
-  accountRoutes(app);
-};
